c0037a3d807dacf06ceec3156229bf6c8a52002b
Samuel-Oyedeji <oyedejisamuel05@gmail.com>
2025-02-28 11:15:43 +0100

    Update provision.sh

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -6,7 +6,7 @@ sudo apt install -y ansible git
 ansible-pull -U https://github.com/Samuel-Oyedeji/DevOps-Stage-4-Infrastructure.git -i localhost, -d /home/ubuntu/ansible
 
 # go to the playbook directory
-cd ansible/ansible
+cd /home/ubuntu/ansible/ansible
 
 # start the playbook
 ansible-playbook playbook.yml
