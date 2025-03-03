b147acd9ecac77b246f9a4785b508b04698c982b
oyedeji samuel <oyedejisamuel05@gmail.com>
2025-03-03 13:10:35 +0100

    changesss

--- terraform/provision.sh
+++ terraform/provision.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+# wait time 
+sleep 20
+
 sudo apt update -y
 sudo apt install -y ansible git
 
+# wait time 
+sleep 20
+
 # Run Ansible Pull to apply configurations from our repo
 ansible-pull -U https://github.com/Samuel-Oyedeji/DevOps-Stage-4-Infrastructure.git -i localhost, -d /home/ubuntu/ansible
+
+# wait time 
+sleep 10
 
 # go to the playbook directory
 cd /home/ubuntu/ansible/ansible
